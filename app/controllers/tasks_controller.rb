dd1f344bcef664273794ea2015b67deb4273401b
toshiki.isokawa <toshi.99.0225@icloud.com>
2021-04-19 07:37:04 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
-        @tasks = Task.all
+       
+        @tasks = current_user.tasks.all
+        
     end
 
     def show
@@ -12,12 +16,13 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
 
         if @task.save
             flash[:success] = 'Task が正常に投稿されました'
             redirect_to @task
         else
+            @task = current_user.tasks.order(id: :desc)
             flash.now[:danger] = 'Task が投稿されませんでした'
             render :new
         end
@@ -41,6 +46,7 @@ class TasksController < ApplicationController
 
         flash[:success] = 'Task は正常に削除されました'
         redirect_to tasks_url
+        
     end
     
     private
@@ -52,6 +58,13 @@ class TasksController < ApplicationController
   # Strong Parameter
     def task_params
         params.require(:task).permit(:content, :status)
-     end
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+            unless @task
+                redirect_to root_url
+            end
+    end
 
 end
