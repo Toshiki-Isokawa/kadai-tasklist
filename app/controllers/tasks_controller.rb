d1f59f29e4d8ffcda934e3acf7a52fac8dec3438
toshiki.isokawa <toshi.99.0225@icloud.com>
2021-04-23 08:44:37 +0000

    modification

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
+    #before_action :correct_user, only: [:edit, :update, :destroy]
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
        
@@ -22,7 +22,6 @@ class TasksController < ApplicationController
             flash[:success] = 'Task が正常に投稿されました'
             redirect_to @task
         else
-            @task = current_user.tasks.order(id: :desc)
             flash.now[:danger] = 'Task が投稿されませんでした'
             render :new
         end
@@ -52,7 +51,10 @@ class TasksController < ApplicationController
     private
     
     def set_task
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(params[:id])
+            unless @task
+                redirect_to root_url
+            end
     end
 
   # Strong Parameter
@@ -60,11 +62,11 @@ class TasksController < ApplicationController
         params.require(:task).permit(:content, :status)
     end
     
-    def correct_user
-        @task = current_user.tasks.find_by(id: params[:id])
-            unless @task
-                redirect_to root_url
-            end
-    end
+    #def correct_user
+        #@task = current_user.tasks.find_by(id: params[:id])
+            #unless @task
+                #redirect_to root_url
+            #end
+    #end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @users = User.order(id: :desc)
   end
 
   def show
-    @user = User.find(params[:id])
-    @tasks = @user.tasks.order(id: :desc)
-    counts(@user)
   end
 
   def new
