e8bda5b96fd7160ef044923399aac6540d6b22ec
toshiki.isokawa <toshi.99.0225@icloud.com>
2021-04-23 09:35:07 +0000

    modification 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
     private
     
     def set_task
-        @task = current_user.tasks.find_by(params[:id])
+        @task = current_user.tasks.find_by(id :params[:id])
             unless @task
                 redirect_to root_url
             end
